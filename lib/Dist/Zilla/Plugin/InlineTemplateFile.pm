475a5a3f905a5160335961e846da11e16dc7eca5
Kent Fredric <kentfredric@gmail.com>
2010-08-28 19:23:51 +1200

    Pass Empty

--- lib/Dist/Zilla/Plugin/InlineTemplateFile.pm
+++ lib/Dist/Zilla/Plugin/InlineTemplateFile.pm
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::InlineTemplateFile;
-# ABSTRACT: Generate Files from Inline Templates
+# ABSTRACT: Generate Files from In-Line Templates
 
 1;
